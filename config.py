826ae327d57a9129a0067137a0489c80a4d20e7d
Nay Min San <69087586+nayminsan123@users.noreply.github.com>
2024-08-03 03:29:55 +0630

    Update g

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 if not __name__.endswith("sample_config"):
     import sys
     print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
-          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
+          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stder
     quit(1
 
 
